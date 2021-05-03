c2a0e4111e2f4597c8e3506ea4f10ea24dca15e1
LamdaDM <mceric1776@gmail.com>
2021-05-04 00:45:11 +1000

    Finishing touches on output and how it accepts arguments

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # gla2h
-Takes 6 arguments:
-1) string to be hashed
-2) add 'timed' if you want the execution time outputted, else leave as 'x'
-3) add 'benchmark' to run through 268 testing iterations of different memory/passes combinations, else leave as 'x'
-4) add memory cost in kilobytes, e.g. 262144 (256mb)
-5) add passes, e.g. 5 passes (2^5 rounds)
-6) add thread count for argon2
+Takes 6 arguments: 
+1) input -> string that will be hashed.
+2) timer -> flag to time how long the hash takes, leave as 'y' if you want the execution time outputted, else leave as 'x' or 'n'.
+3) benchmark -> 'y' to run through 268 testing iterations of different memory/passes combinations, else leave as 'x' or 'n'.
+4) memory cost -> memory to give to the threadpool in megabytes, e.g. '256' (256*1024 kb).
+5) passes -> how many passes or iterations to do, e.g. '10' (2^10 passes).
+6) parallelism -> how many threads to give argon2, usually double the cores the host's CPU has.
--- main.go
+++ main.go
@@ -63,12 +63,12 @@ func GenerateConfig(passes uint32, memory uint32, threads uint8) argon2.Config {
 	}
 }
 
-// Low-level benchmarking func,
+// low-level benchmarking func,
 // uses the loop count for passes and the memtest param for memory count.
 // Starts the timer manually, so timing precision is lowered a bit
 // due to possible time difference between the call of time.Now()
 // and ProduceHash()
-func run(memtest uint32, stdin []byte, timer bool){
+func run(memtest uint32, stdin []byte, timer bool, threads uint8){
 	var startT time.Time
 	var kill bool
 	for i := 3; i <= 70; i++ {
@@ -80,7 +80,7 @@ func run(memtest uint32, stdin []byte, timer bool){
 			startT = time.Now()
 			if hashResult := SafeProduceHash(
 				sinCtx,
-				GenerateConfig(uint32(i), memtest, 12),
+				GenerateConfig(uint32(i), memtest, threads),
 				dummy,
 				timer,
 				startT,
@@ -96,46 +96,50 @@ func run(memtest uint32, stdin []byte, timer bool){
 // Top-level func for benchmarking execution times for hashing.
 // Variables: memory for thread pool (64mb/128mb/256mb/512mb),
 // iterations or passes (3 - 70).
-func benchmark(stdin []byte, timer bool) {
+func benchmark(stdin []byte, timer bool, threads uint8) {
 	const memtest1 uint32 = 64*1024
 	const memtest2 uint32 = 128*1024
 	const memtest3 uint32 = 256*1024
 	const memtest4 uint32 = 512*1024
 
-	run(memtest1, stdin, timer)
-	run(memtest2, stdin, timer)
-	run(memtest3, stdin, timer)
-	run(memtest4, stdin, timer)
+	run(memtest1, stdin, timer, threads)
+	run(memtest2, stdin, timer, threads)
+	run(memtest3, stdin, timer, threads)
+	run(memtest4, stdin, timer, threads)
 
 }
 
 func main() {
-	in := []byte(os.Args[1])
-	var timer bool
 	if len(os.Args) < 7 {
 		fmt.Fprint(os.Stderr, "argument count mismatch, requires 6 args. ")
 		panic(main)
 	}
-	if os.Args[2] == "timed" { timer = true }
-	if os.Args[3] == "benchmark" { benchmark(in, timer)
-	} else {
-		MEM_COST, memParseErr := strconv.ParseUint(os.Args[4], 10, 32)
-			if memParseErr != nil {
-				fmt.Fprint(os.Stderr, memParseErr)
-				panic(memParseErr)
-			}
-		TIME_COST, timeParseErr := strconv.ParseUint(os.Args[5], 10, 32)
-			if timeParseErr != nil {
-				fmt.Fprint(os.Stderr, timeParseErr)
-				panic(timeParseErr)
-			}
-		THREAD_AMOUNT, threadsParseErr := strconv.ParseUint(os.Args[6], 10, 8)
-			if threadsParseErr != nil {
-				fmt.Fprint(os.Stderr, threadsParseErr)
-				panic(threadsParseErr)
-			}
 
-		conf := GenerateConfig(uint32(TIME_COST), uint32(MEM_COST), uint8(THREAD_AMOUNT))
+	in := []byte(os.Args[1])
+	var timer bool
+
+	MEM_COST, memParseErr := strconv.ParseUint(os.Args[4], 10, 32)
+	if memParseErr != nil {
+		fmt.Fprint(os.Stderr, memParseErr)
+		panic(memParseErr)
+	}
+
+	TIME_COST, timeParseErr := strconv.ParseUint(os.Args[5], 10, 32)
+	if timeParseErr != nil {
+		fmt.Fprint(os.Stderr, timeParseErr)
+		panic(timeParseErr)
+	}
+
+	THREAD_AMOUNT, threadsParseErr := strconv.ParseUint(os.Args[6], 10, 8)
+	if threadsParseErr != nil {
+		fmt.Fprint(os.Stderr, threadsParseErr)
+		panic(threadsParseErr)
+	}
+
+	if os.Args[2] == "y" { timer = true }
+	if os.Args[3] == "y" { benchmark(in, timer, uint8(THREAD_AMOUNT))
+	} else {
+		conf := GenerateConfig(uint32(TIME_COST), uint32(MEM_COST) * 1024, uint8(THREAD_AMOUNT))
 
 		res := ProduceHash(conf, in, timer, time.Now())
 		if res.hashErr != nil {
